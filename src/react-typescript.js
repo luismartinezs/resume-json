6200e89c096cdd80de253a439990ce06319b66cb
Luis Martinez <luismartinezwebdev@gmail.com>
2024-07-17 17:28:59 +0700

    up resume

--- src/react-typescript.js
+++ src/react-typescript.js
@@ -4,13 +4,15 @@ import education from "../_partials/education.js";
 import projects from "../_partials/projects.js";
 import skills from "../_partials/skills.js";
 import workExperience from "../_partials/work-experience.js";
-import { filterBySkill, starItemBySkill } from "./util.js";
+import { starItemBySkill } from "./util.js";
 
 export default {
   ...basicInfo,
   jobTitle: "React Typescript Web Developer",
   about:
     "Luis has 5 years of experience as web developer, and 2 years of experience with React and Typescript. He is passionate about crafting clean, well-structured code that follows best practices. Luis stays up to date with industry developments and continuously challenges himself with side projects.",
+  mostAmazingDevelopment:
+    "Audited and enhanced the web accessibility of a complex enterprise application using React and TypeScript, well within allocated budget constraints, for a company valued at $760 million (as of 2021)",
   preferredEnvironment: [
     "React",
     "Typescript",
