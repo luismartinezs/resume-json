8e36f2d2e1856e7a8db2949d63a9e1a8cc29a4e2
Luis Martinez <luismartinezwebdev@gmail.com>
2024-07-17 14:51:29 +0700

    module default

--- generate-json.js
+++ generate-json.js
@@ -4,8 +4,8 @@ import path from 'path';
 ['resume', 'react-typescript'].forEach(async (fileName) => {
   const outputPath = path.join(path.resolve(), `json/${fileName}.json`);
 
-  const data = await import(`./src/${fileName}.js`)
-  fs.writeFileSync(outputPath, JSON.stringify(data, null, 2), 'utf-8');
+  const module = await import(`./src/${fileName}.js`)
+  fs.writeFileSync(outputPath, JSON.stringify(module.default, null, 2), 'utf-8');
 
   console.log(`${fileName}.json has been generated`);
 })
